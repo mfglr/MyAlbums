f178743b8ab63371b51f9ce60b08934ee10863ec
mfglr <mfgglr@outlook.com>
2024-06-18 16:14:32 +0300

    Some test cases has been written.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should redirect to 404 page if user is not found" do
+    user = User.new(id: "test")
+    get user_path(user)
+    assert_redirected_to "/404"
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,37 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+    def setup
+        @email = "test@outlook.com"
+        @username = "test"
+        @name = "test"
+
+        user_saved = users(:ali)
+        @saved_username = user_saved.username
+        @saved_email = user_saved.email
+    end
+
+    test "should not save user if name is empty" do
+        user = User.new(email: @email, username: @username)
+
+        assert_not user.save
+    end
+
+    test "should not save user if email is empty" do
+        user = User.new(name: @name, username: @username)
+
+        assert_not user.save
+    end
+
+    test "should not save user if username is empty" do
+        user = User.new(name: @name, email: @email)
+
+        assert_not user.save
+    end
+
+    test "should not save user if email is not uniq" do
+        user = User.new(name: @name, email: @saved_email, username: @username)
+
+        assert_not user.save
+    end
 end
